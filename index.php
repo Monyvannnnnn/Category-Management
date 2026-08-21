b83c295d63fe43b8211c7c5c8b6ddcae90b0bc27
Munyvann <kimhak.prome@gmail.com>
2026-08-21 11:36:23 +0700

    first commit

--- index.php
+++ index.php
@@ -32,6 +32,9 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <!-- Required for DevExtreme Excel Export -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.4.0/exceljs.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
 
     <!-- jQuery -->
     <link rel="stylesheet" href="styles.css" />
@@ -42,7 +45,7 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
     <script src="https://cdn3.devexpress.com/jslib/23.1.6/js/dx.all.js"></script>
 
     <!-- App CSS Styles -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.0.1">
 
 </head>
 
@@ -76,12 +79,14 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
                     <span class="option-label">Resize Mode:</span>
                     <div id="select-resizing"></div>
                 </div>
-                <div class="search-wrapper">
-
-                    <i class="fa-solid fa-magnifying-glass"></i>
-
-                    <input type="text" id="searchInput" placeholder="Search...">
-
+                <div class="search-and-export">
+                    <button type="button" class="export-icon-btn" id="customExportBtn" title="Export Excel">
+                        <i class="fa-solid fa-file-excel"></i>
+                    </button>
+                    <div class="search-wrapper">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" id="searchInput" placeholder="Search...">
+                    </div>
                 </div>
             </div>
 
@@ -126,8 +131,9 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
             stateStoring: {
                 enabled: true,
                 type: "localStorage",
-                storageKey: "categoryGridStateV3"
+                storageKey: "categoryGridStateV5"
             },
+
             dataSource: new DevExpress.data.CustomStore({
                 key: "id",
                 loadMode: "raw",
@@ -220,7 +226,7 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
                     dataType: "datetime",
                     format: "yyyy-MM-dd HH:mm:ss",
                     allowEditing: false,
-                    cellTemplate: function (container, options) {
+                    cellTemplate: function(container, options) {
                         if (!options.value) {
                             $("<span>").text("-").appendTo(container);
                             return;
@@ -236,11 +242,11 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
                     type: "buttons",
                     caption: "Action",
                     width: 110,
-                    buttons: [
-                        {
+                    buttons: [{
                             name: "edit",
                             template: function(container, options) {
-                                var svg = '<svg fill="none" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle;"><g fill-rule="evenodd" fill="rgb(0,0,0)" fill-rule="evenodd"><path d="m20.6 2c-.3639 0-.7001.11429-.9929.40712l-9.3188 9.31888-.81771 2.8034 2.80351-.8177 9.3188-9.3188c.2146-.2146.4071-.66113.4071-.99291 0-.74771-.6523-1.39999-1.4-1.39999zm-2.4071-1.007095c.7072-.707166 1.571-.992905 2.4071-.992905 1.8523 0 3.4 1.54771 3.4 3.39999 0 .86822-.4075 1.82172-.9929 2.40712l-9.5 9.49999c-.1188.1189-.2657.2058-.4271.2529l-4.8 1.4c-.35053.1022-.72892.0053-.98711-.2529s-.35513-.6366-.25289-.9871l1.39999-4.8c.04707-.1613.13404-.3082.2529-.4271z" fill="currentColor" /><path d="m0 7c0-2.75228 2.24772-5 5-5h6c.5523 0 1 .44772 1 1s-.4477 1-1 1h-6c-1.64772 0-3 1.35228-3 3v12c0 1.6477 1.35228 3 3 3h12c1.6477 0 3-1.3523 3-3v-6c0-.5523.4477-1 1-1s1 .4477 1 1v6c0 2.7523-2.2477 5-5 5h-12c-2.75228 0-5-2.2477-5-5z" fill="currentColor" /></g></svg>';
+                                var svg =
+                                    '<svg fill="none" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle;"><g fill-rule="evenodd" fill="rgb(0,0,0)" fill-rule="evenodd"><path d="m20.6 2c-.3639 0-.7001.11429-.9929.40712l-9.3188 9.31888-.81771 2.8034 2.80351-.8177 9.3188-9.3188c.2146-.2146.4071-.66113.4071-.99291 0-.74771-.6523-1.39999-1.4-1.39999zm-2.4071-1.007095c.7072-.707166 1.571-.992905 2.4071-.992905 1.8523 0 3.4 1.54771 3.4 3.39999 0 .86822-.4075 1.82172-.9929 2.40712l-9.5 9.49999c-.1188.1189-.2657.2058-.4271.2529l-4.8 1.4c-.35053.1022-.72892.0053-.98711-.2529s-.35513-.6366-.25289-.9871l1.39999-4.8c.04707-.1613.13404-.3082.2529-.4271z" fill="currentColor" /><path d="m0 7c0-2.75228 2.24772-5 5-5h6c.5523 0 1 .44772 1 1s-.4477 1-1 1h-6c-1.64772 0-3 1.35228-3 3v12c0 1.6477 1.35228 3 3 3h12c1.6477 0 3-1.3523 3-3v-6c0-.5523.4477-1 1-1s1 .4477 1 1v6c0 2.7523-2.2477 5-5 5h-12c-2.75228 0-5-2.2477-5-5z" fill="currentColor" /></g></svg>';
                                 return $("<a>")
                                     .addClass("dx-link dx-link-edit")
                                     .attr("title", "Edit")
@@ -250,7 +256,8 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
                         {
                             name: "delete",
                             template: function(container, options) {
-                                var svg = '<svg id="Capa_1" enable-background="new 0 0 440 440" height="18" viewBox="0 0 440 440" width="18" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle;"><g><g id="delete_1_"><path d="m412 88h-384c-6.627 0-12-5.373-12-12s5.373-12 12-12h384c6.627 0 12 5.373 12 12s-5.373 12-12 12z" fill="currentColor" /><path d="m316 88h-192c-3.693-.012-7.175-1.723-9.44-4.64-2.234-2.91-3.055-6.663-2.24-10.24l9.92-39.84c4.969-19.547 22.551-33.244 42.72-33.28h110.08c20.169.036 37.751 13.733 42.72 33.28l9.92 39.84c.815 3.577-.006 7.33-2.24 10.24-2.265 2.917-5.747 4.628-9.44 4.64zm-176-24h160l-5.6-24.8c-1.882-9.226-9.945-15.889-19.36-16h-110.08c-9.415.111-17.478 6.774-19.36 16z" fill="currentColor" /><path d="m286.4 440h-132.8c-46.24 0-84.8-29.12-89.76-67.68l-16-231.52c-.442-6.627 4.573-12.358 11.2-12.8s12.358 4.573 12.8 11.2l16 230.72c3.36 25.92 32 46.08 65.92 46.08h132.8c34.24 0 62.56-20.16 65.92-46.88l16-229.92c.442-6.627 6.173-11.642 12.8-11.2s11.642 6.173 11.2 12.8l-16 230.72c-5.28 39.36-44.48 68.48-90.08 68.48z" fill="currentColor" /></g></g></svg>';
+                                var svg =
+                                    '<svg id="Capa_1" enable-background="new 0 0 440 440" height="18" viewBox="0 0 440 440" width="18" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle;"><g><g id="delete_1_"><path d="m412 88h-384c-6.627 0-12-5.373-12-12s5.373-12 12-12h384c6.627 0 12 5.373 12 12s-5.373 12-12 12z" fill="currentColor" /><path d="m316 88h-192c-3.693-.012-7.175-1.723-9.44-4.64-2.234-2.91-3.055-6.663-2.24-10.24l9.92-39.84c4.969-19.547 22.551-33.244 42.72-33.28h110.08c20.169.036 37.751 13.733 42.72 33.28l9.92 39.84c.815 3.577-.006 7.33-2.24 10.24-2.265 2.917-5.747 4.628-9.44 4.64zm-176-24h160l-5.6-24.8c-1.882-9.226-9.945-15.889-19.36-16h-110.08c-9.415.111-17.478 6.774-19.36 16z" fill="currentColor" /><path d="m286.4 440h-132.8c-46.24 0-84.8-29.12-89.76-67.68l-16-231.52c-.442-6.627 4.573-12.358 11.2-12.8s12.358 4.573 12.8 11.2l16 230.72c3.36 25.92 32 46.08 65.92 46.08h132.8c34.24 0 62.56-20.16 65.92-46.88l16-229.92c.442-6.627 6.173-11.642 12.8-11.2s11.642 6.173 11.2 12.8l-16 230.72c-5.28 39.36-44.48 68.48-90.08 68.48z" fill="currentColor" /></g></g></svg>';
                                 return $("<a>")
                                     .addClass("dx-link dx-link-delete")
                                     .attr("title", "Delete")
@@ -311,7 +318,7 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
             searchPanel: {
                 visible: false // Hidden as we use our custom search input
             },
-            onSaved: function (e) {
+            onSaved: function(e) {
                 e.component.refresh();
             }
         });
@@ -328,11 +335,36 @@ if (isset($_GET["action"]) && $_GET["action"] === "read") {
             grid.addRow();
         });
 
+        // Wire up custom Export Excel Button
+        $("#customExportBtn").on("click", function() {
+            try {
+                var gridInstance = $("#gridContainer").dxDataGrid("instance");
+                var workbook = new ExcelJS.Workbook();
+                var worksheet = workbook.addWorksheet('Category');
+
+                DevExpress.excelExporter.exportDataGrid({
+                    component: gridInstance,
+                    worksheet: worksheet,
+                    autoFilterEnabled: true
+                }).then(function() {
+                    workbook.xlsx.writeBuffer().then(function(buffer) {
+                        saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'Categories.xlsx');
+                    });
+                }).catch(function(err) {
+                    alert("Export Error: " + err.message);
+                });
+            } catch (err) {
+                alert("Click Handler Error: " + err.message);
+            }
+        });
+
         // Initialize Column Resizing Mode dxSelectBox
         $('#select-resizing').dxSelectBox({
             items: resizingModes,
             value: localStorage.getItem("categoryGridResizeMode") || resizingModes[0],
-            inputAttr: { 'aria-label': 'Resize Mode' },
+            inputAttr: {
+                'aria-label': 'Resize Mode'
+            },
             width: 250,
             onValueChanged(data) {
                 localStorage.setItem("categoryGridResizeMode", data.value);
